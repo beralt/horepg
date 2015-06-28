d43552a040683fdd02a635071456e19741f49c19
Beralt <beralt@beralt.nl>
2015-06-28 08:41:03 +0200

    put data fetching in a daily run loop

--- horepg.py
+++ horepg.py
@@ -281,21 +281,26 @@ except KeyError as exc:
 switch_user(uid, gid)
 daemonize()
 
-with TVHXMLTVSocket('/home/hts/.hts/tvheadend/epggrab/xmltv.sock') as tvh_client:
-  chmap = ChannelMap()
-  listings = Listings()
-  # add listings for each of the channels
-  for channel_id, channel in chmap.channel_map.items():
-    if channel['title'] in wanted_channels:
-      now = datetime.date.today().timetuple()
-      nr = 0
-      xmltv = XMLTVDocument()
-      xmltv.addChannel(channel_id, channel['title'], channel['images'])
-      for i in range(0, 5):
-        start = int((calendar.timegm(now) + 86400 * i) * 1000) # milis
-        end = start + (86400 * 1000)
-        nr = nr + listings.obtain(xmltv, channel_id, start, end)
-      debug('Adding {:d} programmes for channel {:s}'.format(nr, channel['title']))
-      # send this channel to tvh for processing
-      tvh_client.send(xmltv.document.toprettyxml(encoding='UTF-8'))
+def run_import(wanted_channels):
+  with TVHXMLTVSocket('/home/hts/.hts/tvheadend/epggrab/xmltv.sock') as tvh_client:
+    chmap = ChannelMap()
+    listings = Listings()
+    # add listings for each of the channels
+    for channel_id, channel in chmap.channel_map.items():
+      if channel['title'] in wanted_channels:
+        now = datetime.date.today().timetuple()
+        nr = 0
+        xmltv = XMLTVDocument()
+        xmltv.addChannel(channel_id, channel['title'], channel['images'])
+        for i in range(0, 5):
+          start = int((calendar.timegm(now) + 86400 * i) * 1000) # milis
+          end = start + (86400 * 1000)
+          nr = nr + listings.obtain(xmltv, channel_id, start, end)
+        debug('Adding {:d} programmes for channel {:s}'.format(nr, channel['title']))
+        # send this channel to tvh for processing
+        tvh_client.send(xmltv.document.toprettyxml(encoding='UTF-8'))
 
+if __name__ == '__main__':
+  while True:
+    run_import(wanted_channels)
+    time.sleep(60*60*24)
